58c701af0a417851fb96d0d7172e3933f29fe39a
Will Miao <willmiao.dev@gmail.com>
2022-11-15 23:38:24 +0800

    Add get articles api

--- server/api/article.js
+++ server/api/article.js
@@ -10,8 +10,6 @@ const prisma = new PrismaClient();
 router.get('/', authByToken(false), async (req, res) => {
   const { tag, author, favorited, limit, offset } = req.query;
 
-  console.log({ tag, author, favorited, limit, offset });
-
   const filters = [];
 
   if (tag) {
@@ -19,13 +17,76 @@ router.get('/', authByToken(false), async (req, res) => {
   }
 
   if (author) {
-    filters.push({ User: { name: author } });
+    filters.push({ author: { name: author } });
   }
 
   if (favorited) {
+    filters.push({ favoritedBy: { some: { name: favorited } } });
   }
 
-  res.send('WIP');
+  const take = limit ? Number(limit) : 20;
+  const skip = offset ? Number(offset) : 0;
+
+  const articles = await prisma.article.findMany({
+    where: {
+      AND: filters,
+    },
+    include: {
+      tags: {
+        select: {
+          name: true,
+        },
+      },
+      favoritedBy: {
+        select: {
+          id: true,
+        },
+      },
+      author: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    skip: skip,
+    take: take,
+  });
+
+  const { id, following } = await prisma.user.findFirst({
+    where: {
+      email: req.credential,
+    },
+    select: {
+      id: true,
+      following: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  const output = [];
+  for (const article of articles) {
+    output.push({
+      slug: 'to be implemented',
+      title: article.title,
+      description: article.description,
+      body: article.body,
+      tagList: article.tags.map((tag) => tag['name']),
+      createdAt: article.createdAt,
+      updatedAt: article.updatedAt,
+      favorited: article.favoritedBy.indexOf(id) > 0,
+      favoritesCount: article.favoritedBy.length,
+      author: {
+        username: article.author.name,
+        bio: article.author.bio,
+        image: article.author.image,
+        following: following.indexOf(article.authorId) > 0,
+      },
+    });
+  }
+
+  res.json({ articles: output });
 });
 
 router.get('/feed', authByToken(), async (req, res) => {});
